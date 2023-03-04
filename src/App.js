bc93133c25820a7cc8c8af0748145b062ec71b2b
AAKYURT <abdulkadir.akyurt@computacenter.com>
2023-03-04 13:10:08 +0100

    added some css

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Footer from './components/Footer';
 import data from "./data";
 
 function App() {
-  const xx = <hr className="card-line"/>
+  const line = <hr className="card-line"/>
   const cards = data.map(cardData => {
     return(
       <>
       <Card data={cardData}/>
-      {xx}
+      {line}
       </>
       
     )
